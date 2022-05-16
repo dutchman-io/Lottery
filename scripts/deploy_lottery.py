4fcb06d5a07b770207a59409b24312c889ee54cf
Dutchman <meelisfidelis@gmail.com>
2022-05-16 23:25:56 +0100

    deploy init

---
+++ scripts/deploy_lottery.py
@@ -0,0 +1,9 @@
+from scripts.helpful_script import get_account
+from brownie import accounts, network, Lottery
+
+def deploy_lottery():
+    account = get_account
+    Lottery =Lottery.deploy()
+
+def main():
+    deploy_lottery()
